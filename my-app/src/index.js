321888bec5687fcb306f9303f8433d031914741d
dylanagyemang <105815142+dylanagyemang@users.noreply.github.com>
2022-09-12 12:55:25 -0400

    m

---
+++ my-app/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+//import App from './App';
+
+/*const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);*/
+
+ReactDOM.createrender(
+  <h1>Hello world</h1>
+  ,document.getElementById('root')
+);
