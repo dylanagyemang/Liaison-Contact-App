c060614a1c5e620e3d307e65ae15ae7e2b9250b5
dylanagyemang <105815142+dylanagyemang@users.noreply.github.com>
2022-09-14 09:30:05 -0400

    m

--- liaison/src/index.js
+++ liaison/src/index.js
@@ -1,34 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//ReactDOM.createRoot(document.getElementById('root')).render(<h1>Hello World</h1>);
-//let count = 0
 function Timer(){
   const [count, setCount] = useState(0);
-
   return (<div class='container'>
   <p>{count}</p> <button type='button' class= 'btn btn-warning btn-sm' onClick={()=> setCount(count+1)}>Increase</button>
   </div>
   );
 }
-/*class Counter extends React.Component{
-  state = {
-    counter:0
-  }
-  increase = () =>{
-    this.setState({counter: this.state.counter+1});
 
+class Clout extends React.Component{
+  state = {
+    coun : 0
+  }
+  inc = () => {
+    this.setState({coun:this.state.coun+1});
+  }
+  componentDidMount(){
+    this.setState({coun:66});
   }
   render(){
-    return <div class='container'>
-    <p>{this.state.counter} </p> <button type='button' class= 'btn btn-warning btn-sm' onClick={this.increase}>Increase</button>
+    return <div class='container>'>
+    <p>{this.state.coun}</p><button type='button' class='btn btn-primary btn-sm' onClick={this.inc}>Increase</button>
     </div>;
   }
-}*/
-const timera = <Timer />
-ReactDOM.createRoot(document.getElementById('root')).render(timera,document.getElementById('root'));
-//setInterval(timer,1000);
+}
+
+
+const cnt = <Clout />
+ReactDOM.createRoot(document.getElementById('root')).render(cnt,document.getElementById('root'));
 reportWebVitals();
